537b75725d56380589d35fa7203056619bf79b73
franco-ermacora <ermacora.fran@gmail.com>
2023-05-12 19:18:29 -0300

    week04

---
+++ week04/js/indexChamber.js
@@ -0,0 +1,30 @@
+const hamButton = document.querySelector('#hamButton');
+const burguer = document.querySelector('.burguer');
+
+hamButton.addEventListener('click', () => {
+	burguer.classList.toggle('responsive');
+});
+
+const newdate = new Date(document.lastModified);
+const year = newdate.getFullYear();
+const month = newdate.getMonth() + 1;
+const day = newdate.getDate();
+const hours = newdate.getHours();
+const minutes = newdate.getMinutes();
+const seconds = newdate.getSeconds();
+
+let currentDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+
+document.querySelector("#time").innerHTML = currentDate;
+document.querySelector("#year").innerHTML = year;
+
+
+const datefieldUK = document.querySelector("#actual");
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
